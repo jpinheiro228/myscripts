d6c2c6b3b5e81f024449f50158231ab286b23ea5
Joao Pinheiro <joao.jfnp@gmail.com>
2022-03-26 23:28:14 +0100

    Updating shell_utils
    Added gitconfig file
    Updated zshrc file to use shell_utils
    Updated setup_ubuntu.sh to detect shell, configure GIT and update configs

--- util/setup_ubuntu.sh
+++ util/setup_ubuntu.sh
@@ -1,18 +1,26 @@
 #!/bin/bash
 
 sudo apt update && sudo apt dist-upgrade -y
-
 sudo apt install -y tmux git build-essential nano cmake
 
 if [[ ! -d ~/git-repos/myscripts ]]; then
     mkdir -p ~/git-repos
     git clone https://github.com/jpinheiro228/myscripts.git ~/git-repos/myscripts
+elif [[ -d ~/git-repos/myscripts ]]; then
+    cd ~/git-repos/myscripts && git pull
 fi
 
+ln -sf ~/git-repos/myscripts/configs/gitconfig ~/.gitconfig
 ln -sf ~/git-repos/myscripts/configs/nanorc ~/.nanorc
 ln -sf ~/git-repos/myscripts/configs/tmux.conf ~/.tmux.conf
-ln -sf ~/git-repos/myscripts/configs/aliases ~/.bash_aliases
 
-if [[ -z $(cat ~/.bashrc|grep bash_utils) ]]; then
-    echo "source ~/git-repos/myscripts/configs/bash_utils" >> ~/.bashrc
+MY_SHELL=`grep "^$USER" /etc/passwd |cut -d":" -f7`
+if [[ $MY_SHELL = "/bin/bash" ]]; then
+    if [[ -z $(cat ~/.bashrc|grep shell_utils) ]]; then
+        echo "source ~/git-repos/myscripts/configs/shell_utils" >> ~/.bashrc
+    fi
+elif [[ $MY_SHELL = "/bin/zsh" ]]; then
+    if [[ -z $(cat ~/.zshrc|grep shell_utils) ]]; then
+        echo "source ~/git-repos/myscripts/configs/shell_utils" >> ~/.zshrc
+    fi
 fi
