01961bf9339656e4cea88a20e1ad0363a04b6251
Joao Pinheiro <joao.jfnp@gmail.com>
2022-03-26 23:48:22 +0100

    Minor updates to setup_ubuntu (replace home variable)

--- util/setup_ubuntu.sh
+++ util/setup_ubuntu.sh
@@ -3,24 +3,24 @@
 sudo apt update && sudo apt dist-upgrade -y
 sudo apt install -y tmux git build-essential nano cmake
 
-if [[ ! -d ~/git-repos/myscripts ]]; then
-    mkdir -p ~/git-repos
-    git clone https://github.com/jpinheiro228/myscripts.git ~/git-repos/myscripts
-elif [[ -d ~/git-repos/myscripts ]]; then
-    cd ~/git-repos/myscripts && git pull
+if [[ ! -d $HOME/git-repos/myscripts ]]; then
+    mkdir -p $HOME/git-repos
+    git clone https://github.com/jpinheiro228/myscripts.git $HOME/git-repos/myscripts
+elif [[ -d $HOME/git-repos/myscripts ]]; then
+    cd $HOME/git-repos/myscripts && git pull
 fi
 
-ln -sf ~/git-repos/myscripts/configs/gitconfig ~/.gitconfig
-ln -sf ~/git-repos/myscripts/configs/nanorc ~/.nanorc
-ln -sf ~/git-repos/myscripts/configs/tmux.conf ~/.tmux.conf
+ln -sf $HOME/git-repos/myscripts/configs/gitconfig $HOME/.gitconfig
+ln -sf $HOME/git-repos/myscripts/configs/nanorc $HOME/.nanorc
+ln -sf $HOME/git-repos/myscripts/configs/tmux.conf $HOME/.tmux.conf
 
 MY_SHELL=`grep "^$USER" /etc/passwd |cut -d":" -f7`
 if [[ $MY_SHELL = "/bin/bash" ]]; then
-    if [[ -z $(cat ~/.bashrc|grep shell_utils) ]]; then
-        echo "source ~/git-repos/myscripts/configs/shell_utils" >> ~/.bashrc
+    if [[ -z $(cat $HOME/.bashrc|grep shell_utils) ]]; then
+        echo "source $HOME/git-repos/myscripts/configs/shell_utils" >> $HOME/.bashrc
     fi
 elif [[ $MY_SHELL = "/bin/zsh" ]]; then
-    if [[ -z $(cat ~/.zshrc|grep shell_utils) ]]; then
-        echo "source ~/git-repos/myscripts/configs/shell_utils" >> ~/.zshrc
+    if [[ -z $(cat $HOME/.zshrc|grep shell_utils) ]]; then
+        echo "source $HOME/git-repos/myscripts/configs/shell_utils" >> $HOME/.zshrc
     fi
 fi
